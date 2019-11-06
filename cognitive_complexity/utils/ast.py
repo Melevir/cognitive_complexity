9e679f3ced03965ded067f341f8d063406871a0d
Илья Лебедев <i.lebedev@bestdoctor.ru>
2019-11-06 18:57:58 +0300

    remove extra parenthesis

--- cognitive_complexity/utils/ast.py
+++ cognitive_complexity/utils/ast.py
@@ -7,7 +7,7 @@ from cognitive_complexity.common_types import AnyFuncdef
 
 def has_recursive_calls(funcdef: AnyFuncdef) -> bool:
     return bool([
-        n for n in ast.walk((funcdef))
+        n for n in ast.walk(funcdef)
         if (
             isinstance(n, ast.Call)
             and isinstance(n.func, ast.Name)
